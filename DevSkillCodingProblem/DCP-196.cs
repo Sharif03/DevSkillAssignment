b76f2e7a8999e2c594bd1b843f955edf48293d11
Md. Shariful Islam <abinsu7@gmail.com>
2019-08-02 21:44:36 +0600

    DCP-196.cs problem has been solved.

--- DevSkillCodingProblem/DCP-196.cs
+++ DevSkillCodingProblem/DCP-196.cs
@@ -6,5 +6,16 @@ namespace DevSkillCodingProblem
 {
     class DCP_001
     {
+        static void Main(string[] args)
+        {
+            var T = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                var v = Convert.ToDecimal(Console.ReadLine());
+                var t = Convert.ToDecimal(Console.ReadLine());
+                int r = (int)(v / t);
+                Console.WriteLine(string.Format("-{0:n2}", r));
+            }
+        }
     }
 }
